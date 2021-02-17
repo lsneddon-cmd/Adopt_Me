c5bfb3a47b752b6f478229773ed37b9b307dd204
Lewis Sneddon <>
2021-02-17 13:56:40 +0000

    passed destructured props to Pet

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, name),
+      React.createElement("h1", {}, animal),
+      React.createElement("h1", {}, breed),
+    ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
+      React.createElement(Pet, { name: "Pepper", animal: "Dog", breed: "Cockatiel"}),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed"})
+    ]
+  );
+};
+
+ReactDOM.render(
+  React.createElement(App), 
+  document.getElementById("root")  
+);
