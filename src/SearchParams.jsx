1d9f1ecfaad5a97e08af22bdff89b5b2b063b647
Lewis Sneddon <>
2021-02-17 18:43:34 +0000

    map through array of strings to create option elements

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option value="">All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
