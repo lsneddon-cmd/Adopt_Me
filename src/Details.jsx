30d84984b3bf48b2dddf1cbdbe6b58f7ca03193c
Lewis Sneddon <>
2021-02-18 09:09:30 +0000

    used classProperties syntax instead of ctor

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,13 +3,7 @@ import pet from '@frontendmasters/pet';
 
 class Details extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true
-    };
-  }
+  state = { loading: true };
 
   componentDidMount () {
     pet.animal(this.props.id)
