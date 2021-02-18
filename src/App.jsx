9f268bec470660ceab8c5a3a0ee1c7616a453321
Lewis Sneddon <>
2021-02-18 08:36:55 +0000

    added routing and rendered json in details

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import { render } from 'react-dom';
+import { Router } from '@reach/router';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   )
